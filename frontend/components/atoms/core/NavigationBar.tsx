1a9548d291c4fb539ad4b9538c06206e232837ed
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-08-20 00:34:53 +0900

    added Configure Navigation Title

--- frontend/components/atoms/core/NavigationBar.tsx
+++ frontend/components/atoms/core/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { IoIosSettings } from "react-icons/io";
 
 interface NavigationBarProps {
     menus: {
@@ -13,11 +14,16 @@ interface NavigationBarProps {
 const NavigationBar = ({ menus }: NavigationBarProps) => {
     return (
         <nav className="h-full w-1/4 bg-[#060606] shadow flex flex-col border-gray-50">
+            <h2 className="text-gray-500 mb-4 gap-2 text-lg flex items-center py-6">
+                <IoIosSettings className="text-2xl text-[#E65F2B]" />
+                <span className="text-[#FFFFFF]">設定</span>
+            </h2>
             {
+
                 Object.entries(menus).map(([category, items], index) => (
                     <div key={category} className={`p-2 rounded-[17px] shadow`}>
                         <h3 className="text-gray-500 mb-4 gap-4">{category}</h3>
-                        <ul className="flex flex-col gap-4">
+                        <ul className="flex flex-col gap-4 ">
                             {items.map((menu) => (
                                 <li key={menu.path} className="gap-4">
                                     <Link href={menu.path} className="flex items-center gap-4 hover:bg-gray-300 rounded-md group">
