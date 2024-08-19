ae7773b3c9490c7069ec553dbe9584433d678f8a
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-08-20 00:15:20 +0900

    added hover

--- frontend/components/atoms/core/NavigationBar.tsx
+++ frontend/components/atoms/core/NavigationBar.tsx
@@ -12,17 +12,17 @@ interface NavigationBarProps {
 
 const NavigationBar = ({ menus }: NavigationBarProps) => {
     return (
-        <nav className="h-full w-64 bg-gray-100 flex flex-col">
+        <nav className="h-full w-1/4 bg-[#060606] shadow flex flex-col border-gray-50">
             {
                 Object.entries(menus).map(([category, items], index) => (
-                    <div key={category} className={`${index !== 0 ? 'border-t border-gray-300 pt-4' : ''} my-4`}>
-                        <h3 className="text-sm text-gray-500 mb-2">{category}</h3>
+                    <div key={category} className={`p-2 rounded-[17px] shadow`}>
+                        <h3 className="text-gray-500 mb-2">{category}</h3>
                         <ul>
                             {items.map((menu) => (
                                 <li key={menu.path}>
-                                    <Link href={menu.path} className="flex items-center gap-2 hover:bg-gray-100 my-2 rounded-md">
-                                        {menu.icon}
-                                        {menu.name}
+                                    <Link href={menu.path} className="flex items-center gap-2 hover:bg-gray-300 my-2 rounded-md group">
+                                        <span className="group-hover:text-[#E65F2B] transition-colors">{menu.icon}</span>
+                                        <span className="group-hover:text-[#E65F2B] transition-colors">{menu.name}</span>
                                     </Link>
                                 </li>
                             ))}
