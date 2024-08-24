c5df7ad3ecb2611bda7b2d1ab90ba1d10303bc37
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-08-24 15:26:17 +0900

    adjusted text

--- frontend/components/molecules/NavigationBar.tsx
+++ frontend/components/molecules/NavigationBar.tsx
@@ -17,14 +17,14 @@ interface NavigationBarProps {
 const NavigationBar = ({ menus, title }: NavigationBarProps) => {
     return (
         <nav className="h-full w-1/5 bg-[#060606] shadow flex flex-col border-gray-50">
-            <h2 className="text-gray-500 mb-4 gap-2 text-lg flex items-center py-6">
+            <h2 className="text-gray-500 mb-4 gap-2 text-2xl flex items-center py-6">
                 {title.icon}
                 <span className="text-[#FFFFFF]">{title.name}</span>
             </h2>
             {Object.entries(menus).map(([category, items], index) => (
-                <div key={category} className={`p-2 rounded-[26px] shadow`}>
+                <div key={category} className="p-2 text-lg rounded-[26px] shadow">
                     <h3 className="text-gray-500 mb-4 gap-4">{category}</h3>
-                    <ul className="flex flex-col gap-4">
+                    <ul className="flex flex-col gap-6">
                         {items.map((menu) => (
                             <li key={menu.path} className="gap-4">
                                 <ActiveLink href={menu.path} name={menu.name} icon={menu.icon} />
