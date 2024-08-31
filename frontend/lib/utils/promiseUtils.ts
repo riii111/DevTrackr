965d2e62ca7c5e43fa83885c6c5ea82df7510a11
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-01 01:07:54 +0900

    added TODO comments

--- frontend/lib/utils/promiseUtils.ts
+++ frontend/lib/utils/promiseUtils.ts
@@ -1,4 +1,5 @@
-// 外部からPromiseを制御するための関数
+// 外部からPromiseを制御するための関数.
+// TODO: そのうちモダンで直感的な代替手段に変更する
 export function createExternalPromise() {
   let resolve: (value: void | PromiseLike<void>) => void;
   let reject: (reason?: any) => void;
