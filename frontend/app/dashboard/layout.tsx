326482116a515c6eab9e47275b4883d5b28e0499
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-01 01:08:07 +0900

    set DrawerProvider

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import LayoutDashboardNavigation from "@/components/layouts/dashboard/LayoutDashboardNavigation";
 import LayoutDashboardHeader from "@/components/layouts/dashboard/LayoutDashboardHeader";
+import { DrawerProvider } from "@/lib/store/useDrawerStore";
 
 export default async function DashboardLayout({
   children,
@@ -8,14 +9,16 @@ export default async function DashboardLayout({
 }) {
 
   return (
-    <div className="flex h-screen bg-[#EBDFD7]">
-      <LayoutDashboardNavigation />
-      <div className="flex flex-col flex-grow overflow-hidden">
-        <LayoutDashboardHeader />
-        <main className="flex-row p-8 overflow-y-auto">
-          {children}
-        </main>
+    <DrawerProvider>
+      <div className="flex h-screen bg-[#EBDFD7]">
+        <LayoutDashboardNavigation />
+        <div className="flex flex-col flex-grow overflow-hidden">
+          <LayoutDashboardHeader />
+          <main className="flex-row p-8 overflow-y-auto">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </DrawerProvider>
   );
 }
