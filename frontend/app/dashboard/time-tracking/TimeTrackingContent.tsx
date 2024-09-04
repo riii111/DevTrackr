e46267ec8d37b8a98b8e394952ddcf95ee9bedbc
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-04 16:50:17 +0900

    added ProjectList（Draft）

--- frontend/app/dashboard/time-tracking/TimeTrackingContent.tsx
+++ frontend/app/dashboard/time-tracking/TimeTrackingContent.tsx
@@ -1,9 +1,11 @@
+import { ProjectList } from "@/components/organisms/ProjectList";
 
 export default function TimeTrackingContent({ bgColor }: { bgColor: string }) {
     return (
         <div className={`p-6 rounded-lg ${bgColor} text-text-primary`}>
             <h1 className="text-2xl font-bold mb-4">勤怠</h1>
             <p>ここに勤怠の情報が表示されます。</p>
+            <ProjectList />
         </div>
     );
 }
---
+++ frontend/components/organisms/ProjectList.tsx
@@ -0,0 +1,7 @@
+export const ProjectList = () => {
+    return (
+        <div>
+            <h1>ProjectList</h1>
+        </div>
+    );
+};
