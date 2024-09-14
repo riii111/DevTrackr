1254565c92e6b3011c1181262dc724a809f54999
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-14 21:55:59 +0900

    projectモデルを作成

--- backend/src/models/projects.rs
+++ backend/src/models/projects.rs
@@ -1,11 +1,13 @@
 use bson::oid::ObjectId;
-use chrono::DateTime;
-use chrono_tz::Jst;
+use bson::DateTime as BsonDateTime;
+use chrono::{DateTime, Local, TimeZone};
+use chrono_tz::Asia::Tokyo; // JSTのタイムゾーン
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct WorkingTime {
-    pub start_time: Option<DateTime<Jst>>,
-    pub end_time: Option<DateTime<Jst>>,
+    pub start_time: Option<BsonDateTime>,
+    pub end_time: Option<BsonDateTime>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -17,6 +19,6 @@ pub struct Project {
     pub company_name: String,
     pub working_times: Option<Vec<WorkingTime>>,
     pub total_working_time: Option<i64>,
-    pub created_at: DateTime<Jst>,
-    pub updated_at: Option<DateTime<Jst>>,
+    pub created_at: BsonDateTime,
+    pub updated_at: Option<BsonDateTime>,
 }
