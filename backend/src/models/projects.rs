a3ec6735353e88c702550327314cd77b1605524e
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-16 23:54:05 +0900

    ステータスを追加

--- backend/src/models/projects.rs
+++ backend/src/models/projects.rs
@@ -1,7 +1,15 @@
-use crate::models::working_time::WorkingTime;
 use bson::oid::ObjectId;
 use bson::DateTime as BsonDateTime;
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ProjectStatus {
+    Planning,
+    InProgress,
+    Completed,
+    OnHold,
+    Cancelled,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Project {
@@ -11,6 +19,7 @@ pub struct Project {
     pub skill_labels: Vec<String>,
     pub company_name: String,
     pub working_times: Option<Vec<WorkingTime>>,
+    pub status: ProjectStatus,
     pub total_working_time: Option<i64>,
     pub created_at: BsonDateTime,
     pub updated_at: Option<BsonDateTime>,
