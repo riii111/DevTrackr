1226f800d3e7e8df67ae5890a59e9fe645f8619b
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-16 23:53:44 +0900

    モデルを追加

--- backend/src/models/working_time.rs
+++ backend/src/models/working_time.rs
@@ -1,8 +1,12 @@
+use bson::oid::ObjectId;
 use bson::DateTime as BsonDateTime;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WorkingTime {
+    pub id: ObjectId,
     pub start_time: Option<BsonDateTime>,
     pub end_time: Option<BsonDateTime>,
+    pub created_at: BsonDateTime,
+    pub updated_at: Option<BsonDateTime>,
 }
