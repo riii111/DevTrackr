0644469ac35798f9bcc25a65a836ff4a3d85969d
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-17 10:33:24 +0900

    勤怠コレクションで発生しうるエラーを定義

--- backend/src/errors/mod.rs
+++ backend/src/errors/mod.rs
@@ -1,3 +1,5 @@
 mod project_error;
+mod working_time_error;
 
 pub use project_error::ProjectError;
+pub use working_time_error::WorkingTimeError;
---
+++ backend/src/errors/working_time_error.rs
@@ -0,0 +1,12 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum WorkingTimeError {
+    #[error("勤怠IDが無効です")]
+    InvalidId,
+    #[error("無効な勤務時間: 開始時間が終了時間と同じか後になっています")]
+    InvalidTimeRange,
+    #[error("データベースエラー: {0}")]
+    DatabaseError(#[from] mongodb::error::Error),
+    // 他にエラーがあれば追加
+}
