3fb965eb4bce688dc86856b4d9e976573768c85a
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-18 13:17:14 +0900

    id→_id変換を修正

--- backend/src/models/working_times.rs
+++ backend/src/models/working_times.rs
@@ -6,8 +6,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WorkingTime {
-    #[serde(default = "ObjectId::new")]
-    pub id: ObjectId,
+    #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
+    // app側では"id"として参照できるように
+    pub id: Option<ObjectId>,
     pub start_time: Option<DateTime<Utc>>,
     pub end_time: Option<DateTime<Utc>>,
     #[serde(default = "current_time")]
