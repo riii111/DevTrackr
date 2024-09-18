a800a4ff35f6dc300aadfc07b7ba605e14e29760
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-18 15:03:28 +0900

    （デバッグ用）ログを追加

--- backend/src/endpoints/working_times.rs
+++ backend/src/endpoints/working_times.rs
@@ -7,6 +7,7 @@ use crate::{
 };
 use actix_web::{get, post, put, web, HttpResponse, Responder};
 use bson::oid::ObjectId;
+use log::info;
 use std::sync::Arc;
 
 #[get("/{id}")]
@@ -14,6 +15,8 @@ pub async fn get_working_time(
     usecase: web::Data<WorkingTimeUseCase<MongoWorkingTimeRepository>>,
     id: web::Path<String>,
 ) -> impl Responder {
+    info!("called GET get_working_time!!");
+
     match usecase.get_working_time_by_id(&id).await {
         Ok(Some(working_time)) => HttpResponse::Ok().json(WorkingTimeResponse::from(working_time)),
         Ok(None) => HttpResponse::NotFound().finish(),
@@ -26,6 +29,8 @@ pub async fn create_working_time(
     usecase: web::Data<Arc<WorkingTimeUseCase<MongoWorkingTimeRepository>>>,
     working_time: web::Json<WorkingTimeCreate>,
 ) -> impl Responder {
+    info!("called POST create_working_time!!");
+
     match usecase
         .create_working_time(&working_time.into_inner())
         .await
@@ -46,6 +51,8 @@ pub async fn update_working_time(
     path: web::Path<String>,
     working_time: web::Json<WorkingTimeUpdate>,
 ) -> impl Responder {
+    info!("called updated_working_time!!");
+
     let id = path.into_inner();
     match ObjectId::parse_str(&id) {
         Ok(obj_id) => {
