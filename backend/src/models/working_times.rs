c916a62a1588a347bb520bc9f1af5640bccf5715
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-18 11:53:18 +0900

    UTCで統一

--- backend/src/models/working_times.rs
+++ backend/src/models/working_times.rs
@@ -1,14 +1,22 @@
 use bson::oid::ObjectId;
-use bson::DateTime as BsonDateTime;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+
+// memo: バックエンド側では時刻をUTCで統一し、フロント側で変換する事を想定.
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WorkingTime {
+    #[serde(default = "ObjectId::new")]
     pub id: ObjectId,
-    pub start_time: Option<BsonDateTime>,
-    pub end_time: Option<BsonDateTime>,
-    pub created_at: BsonDateTime,
-    pub updated_at: Option<BsonDateTime>,
+    pub start_time: Option<DateTime<Utc>>,
+    pub end_time: Option<DateTime<Utc>>,
+    #[serde(default = "current_time")]
+    pub created_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
+
+fn current_time() -> DateTime<Utc> {
+    Utc::now()
 }
 
 // TODO: 単体バリデーション追加する（未来の時刻が入力されていないか、など）
