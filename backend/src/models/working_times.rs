f8a67b13ceb8bb9cf976c8b7bb838ecd9d6721ef
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-18 16:04:17 +0900

    未使用なので削除

--- backend/src/models/working_times.rs
+++ backend/src/models/working_times.rs
@@ -5,24 +5,6 @@ use serde::{Deserialize, Serialize};
 // memo: バックエンド側では時刻をUTCで統一し、フロント側で変換する事を想定.
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct WorkingTime {
-    // app側では"id"として参照できるように
-    #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    pub start_time: Option<DateTime<Utc>>,
-    pub end_time: Option<DateTime<Utc>>,
-    #[serde(default = "current_time")]
-    pub created_at: DateTime<Utc>,
-    pub updated_at: Option<DateTime<Utc>>,
-}
-
-fn current_time() -> DateTime<Utc> {
-    Utc::now()
-}
-
-// TODO: 単体バリデーション追加する（未来の時刻が入力されていないか、など）
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct WorkingTimeCreate {
     pub start_time: DateTime<Utc>,
     pub end_time: DateTime<Utc>,
@@ -44,3 +26,5 @@ pub struct WorkingTimeInDB {
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
 }
+
+// TODO: 単体バリデーション追加する（未来の時刻が入力されていないか、など）
