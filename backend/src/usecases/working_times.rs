2b17340e25b7559031090af1daf95b2b9c2e2ea3
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-25 18:26:50 +0900

    追加→enqueueするロジック（Draft

--- backend/src/usecases/working_times.rs
+++ backend/src/usecases/working_times.rs
@@ -1,21 +1,37 @@
-use crate::adapters::async_queue::AsyncQueueAdapter;
+// use crate::adapters::async_queue_adapter::AsyncQueueAdapter;
 use crate::errors::app_error::AppError;
 use crate::errors::repositories_error::RepositoryError;
+// use crate::models::projects::{MonthlyWorkingTime, ProjectInDB, ProjectUpdate};
 use crate::models::working_times::{WorkingTimeCreate, WorkingTimeInDB, WorkingTimeUpdate};
+// use crate::repositories::projects::MongoProjectRepository;
 use crate::repositories::working_times::WorkingTimeRepository;
-use bson::oid::ObjectId;
+// use crate::usecases::projects::ProjectUseCase;
+use bson::{oid::ObjectId, DateTime as BsonDateTime};
 use std::sync::Arc;
+
+// WorkingTimeCreate と WorkingTimeUpdate から共通のフィールドを取り出すヘルパー関数
+// fn get_working_time_data(start_time: &BsonDateTime, end_time: &Option<BsonDateTime>) -> i64 {
+//     end_time.map_or(0, |end_time| {
+//         (end_time.to_chrono() - start_time.to_chrono()).num_seconds()
+//     })
+// }
 
 pub struct WorkingTimeUseCase<R: WorkingTimeRepository> {
     repository: Arc<R>,
-    queue_adapter: AsyncQueueAdapter,
+    // queue_adapter: Arc<AsyncQueueAdapter>,
+    // project_usecase: Arc<ProjectUseCase<MongoProjectRepository>>,
 }
 
 impl<R: WorkingTimeRepository> WorkingTimeUseCase<R> {
-    pub fn new(repository: Arc<R>, queue_adapter: AsyncQueueAdapter) -> Self {
+    pub fn new(
+        repository: Arc<R>,
+        // queue_adapter: Arc<AsyncQueueAdapter>,
+        // project_usecase: Arc<ProjectUseCase<MongoProjectRepository>>,
+    ) -> Self {
         Self {
             repository,
-            queue_adapter,
+            // queue_adapter,
+            // project_usecase,
         }
     }
 
@@ -48,24 +64,30 @@ impl<R: WorkingTimeRepository> WorkingTimeUseCase<R> {
             }
         }
 
+        // enqueue前にproject_idをもとにprojectデータを取得
+        // let project = self
+        //     .project_usecase
+        //     .get_project_by_id(&working_time.project_id.to_hex())
+        //     .await?
+        //     .ok_or(AppError::ValidationError(
+        //         "更新対象のプロジェクトが存在しません".to_string(),
+        //     ))?;
+
+        // let duration = get_working_time_data(&working_time.start_time, &working_time.end_time);
+
+        // キューにイベントを追加
+        // if let Err(e) = self.queue_adapter.enqueue(project, duration).await {
+        //     log::error!("Failed to enqueue update event: {}", e);
+        // }
+
         let result = self
             .repository
-            .insert_one(&working_time)
+            .insert_one(working_time)
             .await
             .map_err(|e| match e {
                 RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
                 RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
             })?;
-
-        // キューにイベントを追加
-        if let Err(e) = self
-            .queue_adapter
-            .enqueue(working_time.project_id, calculate_duration(working_time))
-            .await
-        {
-            log::error!("Failed to enqueue update event: {}", e);
-            // ここでエラーを返すかどうかはビジネスロジック次第
-        }
 
         Ok(result)
     }
@@ -99,21 +121,21 @@ impl<R: WorkingTimeRepository> WorkingTimeUseCase<R> {
             ));
         }
 
-        // 対象のプロジェクトが存在するか
-        if self
-            .repository
-            .find_by_id(&working_time.project_id)
-            .await
-            .map_err(|e| match e {
-                RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
-                RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
-            })?
-            .is_none()
-        {
-            return Err(AppError::ValidationError(
-                "更新対象のプロジェクトが存在しません".to_string(),
-            ));
-        }
+        // project_idをもとにprojectデータを取得
+        // let project = self
+        //     .project_usecase
+        //     .get_project_by_id(&working_time.project_id.to_hex())
+        //     .await?
+        //     .ok_or(AppError::ValidationError(
+        //         "更新対象のプロジェクトが存在しません".to_string(),
+        //     ))?;
+
+        // let duration_diff = get_working_time_data(&working_time.start_time, &working_time.end_time);
+
+        // キューにイベントを追加
+        // if let Err(e) = self.queue_adapter.enqueue(project, duration_diff).await {
+        //     log::error!("Failed to enqueue update event: {}", e);
+        // }
 
         let result = self
             .repository
@@ -124,35 +146,42 @@ impl<R: WorkingTimeRepository> WorkingTimeUseCase<R> {
                 RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
             })?;
 
-        // キューにイベントを追加（更新の場合）
-        if let Err(e) = self
-            .queue_adapter
-            .enqueue(
-                working_time.project_id,
-                calculate_duration_diff(working_time),
-            )
-            .await
-        {
-            log::error!("Failed to enqueue update event: {}", e);
-            // ここでエラーを返すかどうかはビジネスロジック次第
-        }
-
         Ok(result)
     }
-}
 
-fn calculate_duration(working_time: &WorkingTimeCreate) -> i64 {
-    log::info!("called calculate_duration");
-    // 開始時間と終了時間から稼働時間を計算するロジック
-    working_time.end_time.map_or(0, |end_time| {
-        (end_time.to_chrono() - working_time.start_time.to_chrono()).num_seconds()
-    })
-}
+    // pub async fn update_total_working_time(&self, project: &ProjectInDB) -> Result<(), AppError> {
+    //     let working_times = self
+    //         .repository
+    //         .find_all_by_project_id(&project.id.unwrap())
+    //         .await
+    //         .map_err(|e| match e {
+    //             RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
+    //             RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
+    //         })?;
 
-fn calculate_duration_diff(working_time: &WorkingTimeUpdate) -> i64 {
-    log::info!("called calculate_duration_diff");
-    // 更新された稼働時間の差分を計算するロジック
-    working_time.end_time.map_or(0, |end_time| {
-        (end_time.to_chrono() - working_time.start_time.to_chrono()).num_seconds()
-    })
+    //     if working_times.is_empty() {
+    //         return Err(AppError::NotFound(
+    //             "プロジェクトに関連する稼働時間が見つかりません".to_string(),
+    //         ));
+    //     }
+
+    //     // 総稼働時間を計算
+    //     let total_working_time: i64 = working_times
+    //         .iter()
+    //         .map(|wt| get_working_time_data(&wt.start_time, &wt.end_time))
+    //         .sum();
+
+    //     // プロジェクトを更新
+    //     let update = ProjectUpdate {
+    //         total_working_time: Some(total_working_time),
+    //         // 他のフィールドは更新しない
+    //         ..Default::default()
+    //     };
+
+    //     self.project_usecase
+    //         .update_project(&project.id.unwrap(), &update)
+    //         .await?;
+
+    //     Ok(())
+    // }
 }
