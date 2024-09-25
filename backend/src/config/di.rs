32e5be5af62b3a69a3a11240d576724485aa8638
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-25 18:25:46 +0900

    キューの初期化（Draft

--- backend/src/config/di.rs
+++ backend/src/config/di.rs
@@ -1,4 +1,4 @@
-use crate::adapters::async_queue::{AsyncQueueAdapter, UpdateEvent};
+// use crate::adapters::async_queue_adapter::{AsyncQueueAdapter, UpdateEvent};
 use crate::repositories::projects::MongoProjectRepository;
 use crate::repositories::working_times::MongoWorkingTimeRepository;
 use crate::usecases::projects::ProjectUseCase;
@@ -7,21 +7,23 @@ use mongodb::Database;
 use std::sync::Arc;
 use tokio::sync::mpsc;
 
-pub fn init_async_queue() -> (AsyncQueueAdapter, mpsc::Receiver<UpdateEvent>) {
-    let (sender, receiver) = mpsc::channel(100);
-    let queue_adapter = AsyncQueueAdapter::new(sender);
-    (queue_adapter, receiver)
-}
+// pub fn init_async_queue() -> (AsyncQueueAdapter, mpsc::Receiver<UpdateEvent>) {
+//     let (sender, receiver) = mpsc::channel(100);
+//     let queue_adapter = AsyncQueueAdapter::new(sender);
+//     (queue_adapter, receiver)
+// }
 
 // working_time
 pub fn init_working_time_usecase(
     db: Arc<Database>,
-    queue_adapter: AsyncQueueAdapter,
+    // queue_adapter: Arc<AsyncQueueAdapter>,
+    // project_usecase: Arc<ProjectUseCase<MongoProjectRepository>>,
 ) -> Arc<WorkingTimeUseCase<MongoWorkingTimeRepository>> {
     let working_time_repository = Arc::new(MongoWorkingTimeRepository::new(db));
     Arc::new(WorkingTimeUseCase::new(
         working_time_repository,
-        queue_adapter,
+        // queue_adapter,
+        // project_usecase,
     ))
 }
 
