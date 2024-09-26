e072e0ec14d2f3f3a93ce9957257b62bdad4d04c
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-26 16:46:21 +0900

    コメント追加
    InDBにのみコメント追加し上の順番に（目に入りやすいよう）

--- backend/src/models/companies.rs
+++ backend/src/models/companies.rs
@@ -27,8 +27,34 @@ pub struct AnnualRevenue {
 
 #[derive(Serialize, Deserialize, Debug, ToSchema)]
 pub struct Bonus {
-    pub amount: i64,
+    pub amount: i64,    // 金額(円)
     pub frequency: i32, // 月数で表現（例：6は6ヶ月に1回）
+}
+
+#[derive(Serialize, Deserialize, Debug, ToSchema)]
+pub struct CompanyInDB {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    #[schema(value_type = String, example = "507f1f77bcf86cd799439011")]
+    pub id: Option<ObjectId>,
+    pub company_name: String,                       // 企業名
+    pub establishment_year: i32,                    // 設立年
+    pub headquarters_location: String,              // 本社所在地
+    pub website_url: String,                        // 企業の公式サイトURL
+    pub employee_count: i32,                        // 従業員数
+    pub annual_revenue: Option<AnnualRevenue>,      // 年間売上
+    pub affiliation_start_date: BsonDateTime,       // 契約開始日
+    pub affiliation_end_date: Option<BsonDateTime>, // 契約終了日
+    pub contract_type: ContractType,                // 契約タイプ
+    pub major_clients: Option<Vec<String>>,         // 主要顧客
+    pub major_services: Option<Vec<String>>,        // 主要サービス
+    pub average_hourly_rate: Option<i32>,           // 平均時給
+    pub bonus: Option<Bonus>,                       // ボーナス
+    #[serde(default = "default_company_status")]
+    pub status: CompanyStatus, // ユーザーとの契約ステータス
+    #[schema(value_type = String, example = "2023-04-13T12:34:56Z")]
+    pub created_at: BsonDateTime,
+    #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
+    pub updated_at: Option<BsonDateTime>,
 }
 
 #[derive(Deserialize, Debug, ToSchema)]
@@ -39,7 +65,6 @@ pub struct CompanyCreate {
     pub website_url: String,
     pub employee_count: i32,
     pub annual_revenue: Option<AnnualRevenue>,
-    pub remote_work_policy: Option<String>,
     pub affiliation_start_date: BsonDateTime,
     pub affiliation_end_date: Option<BsonDateTime>,
     pub contract_type: ContractType,
@@ -59,7 +84,6 @@ pub struct CompanyUpdate {
     pub website_url: Option<String>,
     pub employee_count: Option<i32>,
     pub annual_revenue: Option<AnnualRevenue>,
-    pub remote_work_policy: Option<String>,
     pub affiliation_start_date: Option<BsonDateTime>,
     pub affiliation_end_date: Option<BsonDateTime>,
     pub contract_type: Option<ContractType>,
@@ -68,33 +92,6 @@ pub struct CompanyUpdate {
     pub average_hourly_rate: Option<i32>,
     pub bonus: Option<Bonus>,
     pub status: Option<CompanyStatus>,
-}
-
-#[derive(Serialize, Deserialize, Debug, ToSchema)]
-pub struct CompanyInDB {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    #[schema(value_type = String, example = "507f1f77bcf86cd799439011")]
-    pub id: Option<ObjectId>,
-    pub company_name: String,
-    pub establishment_year: i32,
-    pub headquarters_location: String,
-    pub website_url: String,
-    pub employee_count: i32,
-    pub annual_revenue: Option<AnnualRevenue>,
-    pub remote_work_policy: Option<String>,
-    pub affiliation_start_date: BsonDateTime,
-    pub affiliation_end_date: Option<BsonDateTime>,
-    pub contract_type: ContractType,
-    pub major_clients: Option<Vec<String>>,
-    pub major_services: Option<Vec<String>>,
-    pub average_hourly_rate: Option<i32>,
-    pub bonus: Option<Bonus>,
-    #[serde(default = "default_company_status")]
-    pub status: CompanyStatus,
-    #[schema(value_type = String, example = "2023-04-13T12:34:56Z")]
-    pub created_at: BsonDateTime,
-    #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
-    pub updated_at: Option<BsonDateTime>,
 }
 
 fn default_company_status() -> CompanyStatus {
@@ -110,7 +107,6 @@ impl From<CompanyInDB> for CompanyUpdate {
             website_url: Some(company.website_url),
             employee_count: Some(company.employee_count),
             annual_revenue: company.annual_revenue,
-            remote_work_policy: company.remote_work_policy,
             affiliation_start_date: Some(company.affiliation_start_date),
             affiliation_end_date: company.affiliation_end_date,
             contract_type: Some(company.contract_type),
