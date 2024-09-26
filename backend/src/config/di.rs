16385d735592c0a3072e7501ce67b0895dec5505
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-26 17:40:52 +0900

    company usecaseの作成

--- backend/src/config/di.rs
+++ backend/src/config/di.rs
@@ -1,5 +1,7 @@
+use crate::repositories::companies::MongoCompanyRepository;
 use crate::repositories::projects::MongoProjectRepository;
 use crate::repositories::work_logs::MongoWorkLogsRepository;
+use crate::usecases::companies::CompanyUseCase;
 use crate::usecases::projects::ProjectUseCase;
 use crate::usecases::work_logs::WorkLogsUseCase;
 use mongodb::Database;
@@ -18,4 +20,10 @@ pub fn init_work_logs_usecase(
 pub fn init_project_usecase(db: &Database) -> Arc<ProjectUseCase<MongoProjectRepository>> {
     let project_repository = Arc::new(MongoProjectRepository::new(db));
     Arc::new(ProjectUseCase::new(project_repository))
+}
+
+// company
+pub fn init_company_usecase(db: &Database) -> Arc<CompanyUseCase<MongoCompanyRepository>> {
+    let company_repository = Arc::new(MongoCompanyRepository::new(db));
+    Arc::new(CompanyUseCase::new(company_repository))
 }
---
+++ backend/src/usecases/companies.rs
@@ -0,0 +1,68 @@
+use crate::errors::app_error::AppError;
+use crate::errors::repositories_error::RepositoryError;
+use crate::models::companies::{CompanyCreate, CompanyInDB, CompanyUpdate};
+use crate::repositories::companies::CompanyRepository;
+use bson::oid::ObjectId;
+use std::sync::Arc;
+
+pub struct CompanyUseCase<R: CompanyRepository> {
+    repository: Arc<R>,
+}
+
+impl<R: CompanyRepository> CompanyUseCase<R> {
+    pub fn new(repository: Arc<R>) -> Self {
+        Self { repository }
+    }
+
+    pub async fn get_company_by_id(&self, id: &str) -> Result<Option<CompanyInDB>, AppError> {
+        let object_id = ObjectId::parse_str(id)
+            .map_err(|_| AppError::BadRequest("無効なIDです".to_string()))?;
+        self.repository
+            .find_by_id(&object_id)
+            .await
+            .map_err(|e| match e {
+                RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
+                RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
+            })
+    }
+
+    pub async fn create_company(&self, company: CompanyCreate) -> Result<ObjectId, AppError> {
+        self.repository
+            .insert_one(company)
+            .await
+            .map_err(|e| match e {
+                RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
+                RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
+            })
+    }
+
+    pub async fn update_company_by_id(
+        &self,
+        id: &ObjectId,
+        company: &CompanyUpdate,
+    ) -> Result<bool, AppError> {
+        // 既存のドキュメントが存在するか確認
+        if self
+            .repository
+            .find_by_id(id)
+            .await
+            .map_err(|e| match e {
+                RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
+                RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
+            })?
+            .is_none()
+        {
+            return Err(AppError::NotFound(
+                "更新対象の会社が見つかりません".to_string(),
+            ));
+        }
+
+        self.repository
+            .update_one(*id, company)
+            .await
+            .map_err(|e| match e {
+                RepositoryError::ConnectionError => AppError::DatabaseConnectionError,
+                RepositoryError::DatabaseError(err) => AppError::DatabaseError(err),
+            })
+    }
+}
