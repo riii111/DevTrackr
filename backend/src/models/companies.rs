2e706f4058e67a49bfaff68886de3504bf055dfb
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-09-27 09:25:59 +0900

    サンプル値とコメントを追加

--- backend/src/models/companies.rs
+++ backend/src/models/companies.rs
@@ -53,59 +53,65 @@ pub enum ContractType {
 #[derive(Serialize, Deserialize, Debug, Validate, ToSchema)]
 pub struct AnnualRevenue {
     #[validate(range(min = 0, message = "年間売上は0以上である必要があります"))]
-    pub amount: i64,
+    pub amount: i64, // 年間売上
     #[validate(range(max = 2100, message = "会計年度は2100年以前である必要があります"))]
-    pub fiscal_year: i32,
+    pub fiscal_year: i32, // 会計年度
 }
 
 #[derive(Serialize, Deserialize, Debug, Validate, ToSchema)]
 pub struct Bonus {
     #[validate(range(min = 0, message = "ボーナス金額は0以上である必要があります"))]
-    pub amount: i64,
+    pub amount: i64, // ボーナス金額
     #[validate(range(
         min = 1,
         max = 12,
         message = "ボーナス頻度は1から12の間である必要があります"
     ))]
-    pub frequency: i32,
+    pub frequency: i32, // ボーナス頻度
 }
 
 // TODO: 共通フィールドの扱い方については要検討。あくまでもモデル実装内部の話なのに、repositoriesやresponseで"common.""と記述するのが面倒
 #[derive(Serialize, Deserialize, Debug, Validate, ToSchema)]
 pub struct CompanyCommon {
-    #[validate(length(
-        min = 2,
-        max = 100,
-        message = "会社名は2〜100文字である必要���あります"
-    ))]
-    pub company_name: String,
+    #[validate(length(min = 2, max = 100, message = "会社名は2〜100文字である必要あります"))]
+    #[schema(example = "株式会社テスト")]
+    pub company_name: String, // 企業名
     #[validate(range(
         min = 1800,
         max = 2100,
         message = "設立年は1800年から現在までの間である必要があります"
     ))]
-    pub establishment_year: i32,
+    #[schema(example = 2024)]
+    pub establishment_year: i32, // 設立年
     #[validate(length(max = 200, message = "本社所在地は200文字以内である必要があります"))]
-    pub headquarters_location: String,
+    #[schema(example = "東京都千代田区")]
+    pub headquarters_location: String, // 本社所在地
     #[validate(url(message = "有効なURLを入力してください"))]
-    pub website_url: String,
+    #[schema(example = "https://www.example.com")]
+    pub website_url: String, // 企業サイトURL
     #[validate(range(min = 1, message = "従業員数は1以上である必要があります"))]
-    pub employee_count: i32,
-    pub annual_revenue: Option<AnnualRevenue>,
-    pub contract_type: ContractType,
+    #[schema(example = 100)]
+    pub employee_count: i32, // 従業員数
+    pub annual_revenue: Option<AnnualRevenue>, // 年間売上
+    pub contract_type: ContractType,           // 契約タイプ
     #[validate(length(max = 10, message = "主要顧客は最大10件まで登録できます"))]
-    pub major_clients: Option<Vec<String>>,
+    #[schema(example = "金融,IT,製造")]
+    pub major_clients: Option<Vec<String>>, // 主要顧客
     #[validate(length(max = 10, message = "主要サービスは最大10件まで登録できます"))]
-    pub major_services: Option<Vec<String>>,
+    #[schema(example = "システム開発,ネットワークセキュリティ,データ分析")]
+    pub major_services: Option<Vec<String>>, // 主要サービス
     #[validate(range(
         min = 500,
         max = 100000,
         message = "平均時給は500円から100,000円の間である必要があります"
     ))]
-    pub average_hourly_rate: Option<i32>,
-    pub bonus: Option<Bonus>,
+    #[schema(example = 4000)]
+    pub average_hourly_rate: Option<i32>, // 平均時給
+    #[schema(example = json!({"amount": 100000, "frequency": 1}))]
+    pub bonus: Option<Bonus>, // ボーナス
     #[serde(default = "default_company_status")]
-    pub status: CompanyStatus,
+    #[schema(example = "PendingContract")]
+    pub status: CompanyStatus, // 契約ステータス
 }
 
 #[derive(Serialize, Deserialize, Debug, Validate, ToSchema)]
@@ -120,9 +126,9 @@ pub struct CompanyInDB {
     #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
     pub affiliation_end_date: Option<BsonDateTime>, // 契約終了日
     #[schema(value_type = String, example = "2023-04-13T12:34:56Z")]
-    pub created_at: BsonDateTime,
+    pub created_at: BsonDateTime, // 作成日時
     #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
-    pub updated_at: Option<BsonDateTime>,
+    pub updated_at: Option<BsonDateTime>, // 更新日時
 }
 
 #[derive(Serialize, Deserialize, Debug, ToSchema)]
@@ -130,9 +136,9 @@ pub struct CompanyCreate {
     #[serde(flatten)]
     pub common: CompanyCommon,
     #[schema(value_type = String, example = "2023-04-13T12:34:56Z")]
-    pub affiliation_start_date: BsonDateTime,
+    pub affiliation_start_date: BsonDateTime, // 契約開始日
     #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
-    pub affiliation_end_date: Option<BsonDateTime>,
+    pub affiliation_end_date: Option<BsonDateTime>, // 契約終了日
 }
 
 impl CompanyCreate {
@@ -159,9 +165,9 @@ pub struct CompanyUpdate {
     #[serde(flatten)]
     pub common: CompanyCommon,
     #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
-    pub affiliation_start_date: Option<BsonDateTime>,
+    pub affiliation_start_date: Option<BsonDateTime>, // 契約開始日
     #[schema(value_type = Option<String>, example = "2023-04-13T12:34:56Z")]
-    pub affiliation_end_date: Option<BsonDateTime>,
+    pub affiliation_end_date: Option<BsonDateTime>, // 契約終了日
 }
 
 impl CompanyUpdate {
