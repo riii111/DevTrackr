5bd8be7dc2371086706d5a221ad8824acc235d33
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-01 19:33:44 +0900

    不要な処理を削除
    対象のHTTPメソッドはクレートが自動で適用してくれている

--- backend/src/middleware/csrf.rs
+++ backend/src/middleware/csrf.rs
@@ -1,12 +1,10 @@
 use actix_csrf::CsrfMiddleware;
 use actix_web::cookie::SameSite;
-use actix_web::http::Method;
 use rand::rngs::StdRng;
 
+// https://kvnallsn.github.io/actix-web-database-identity/actix_web/middleware/csrf/index.html
 pub fn csrf_middleware() -> CsrfMiddleware<StdRng> {
     CsrfMiddleware::<StdRng>::new()
-        .set_cookie(Method::GET, "/login") // クッキーを設定するエンドポイント
-        .set_cookie(Method::GET, "/register")
         .http_only(true) // JavaScriptからのアクセスを防ぐ
         .secure(true) // HTTPS接続でのみクッキーを送信
         .same_site(Some(SameSite::Strict)) // 同一サイトからのリクエストのみクッキーを送信
