d3bd7cadf641893d09ec3b0ede394c8194c96dcf
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-10 14:21:04 +0900

    タイトル、アカウントアイコンを両端に修正

--- frontend/components/layouts/dashboard/LayoutDashboardHeader.tsx
+++ frontend/components/layouts/dashboard/LayoutDashboardHeader.tsx
@@ -2,23 +2,19 @@ import dynamic from 'next/dynamic';
 
 const DynamicAccountMenu = dynamic(() => import('@/components/molecules/core/AccountMenu'), {
     ssr: false,
-    loading: () => <div className="w-8 h-8 bg-gray-200 rounded-full animate-pulse"></div>
+    loading: () => <div className="w-8 h-8 bg-main-bg rounded-full animate-pulse"></div>
 });
 
 const DynamicPageTitle = dynamic(() => import('@/components/molecules/PageTitle'), {
     ssr: false,
-    loading: () => <div className="h-6 w-32 bg-gray-200 animate-pulse rounded"></div>
+    loading: () => <div className="h-6 w-32 bg-main-bg animate-pulse rounded"></div>
 });
 
 const LayoutDashboardHeader = () => {
     return (
-        <header className="w-full h-16 border-b border-gray-400">
-            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-full">
-                <DynamicPageTitle />
-                <div className="ml-auto">
-                    <DynamicAccountMenu />
-                </div>
-            </nav>
+        <header className="flex items-center justify-between px-4 py-2 border-b border-gray-400">
+            <DynamicPageTitle />
+            <DynamicAccountMenu />
         </header>
     );
 };
