65860dfd61c6a825bda3f9cb935ab2b0d0c8688a
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-14 12:52:45 +0900

    リージョンを環境変数化

--- backend/src/config/s3.rs
+++ backend/src/config/s3.rs
@@ -13,11 +13,12 @@ pub async fn init_s3_client() -> Result<Arc<Client>, Box<dyn Error>> {
         std::env::var("MINIO_ACCESS_KEY").expect("MINIO_ACCESS_KEYが設定されていません");
     let minio_secret_key =
         std::env::var("MINIO_SECRET_KEY").expect("MINIO_SECRET_KEYが設定されていません");
+    let region = std::env::var("S3_REGION").expect("S3_REGIONが設定されていません");
 
     let creds = Credentials::new(minio_access_key, minio_secret_key, None, None, "minio");
 
     let config = Builder::new()
-        .region(Region::new("us-east-1"))
+        .region(Region::new(region))
         .endpoint_url(minio_endpoint)
         .credentials_provider(creds)
         .force_path_style(true)
