bc3b56c899ef66af5cc3ee1e1e76f025a0b1e7e8
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-17 11:02:23 +0900

    スタイル変更

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -10,11 +10,11 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-white shadow hover:bg-secondary",
+          "bg-primary text-white shadow hover:bg-primary/80",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-secondary/80 hover:text-accent-foreground",
+          "border border-input bg-white text-primary shadow-sm hover:bg-secondary/80 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-secondary hover:text-accent-foreground",
