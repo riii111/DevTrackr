a917729c01e4f71b484b3bae767e6fa3fa75c575
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-19 16:59:39 +0900

    ナビゲーションバーの横幅を固定

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -9,14 +9,13 @@ export default async function DashboardLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
-
   return (
     <DrawerProvider>
       <div className="bg-main-bg text-foreground h-screen flex">
         <LayoutDashboardNavigation />
-        <div className="flex flex-col flex-grow overflow-hidden">
+        <div className="flex flex-col flex-grow min-w-0 overflow-hidden">
           <LayoutDashboardHeader />
-          <main className="flex-row p-8 overflow-y-auto bg-main-bg">
+          <main className="flex-grow p-8 overflow-auto bg-main-bg">
             {children}
             {modal}
           </main>
