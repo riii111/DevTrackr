ed870f175960b1bcfaad86969265e6be32ac6e23
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-23 15:28:19 +0900

    使わないので削除

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -22,11 +22,7 @@ export async function middleware(request: NextRequest) {
     let accessToken = request.cookies.get("access_token");
 
     // アクセストークンが存在しない、または無効（期限切れを含む）の場合
-    if (
-      !accessToken ||
-      !accessToken.value ||
-      isTokenExpired(accessToken.value)
-    ) {
+    if (!accessToken || !accessToken.value) {
       try {
         // リフレッシュトークンを使用してアクセストークンを更新
         const response = await fetch(
@@ -53,20 +49,6 @@ export async function middleware(request: NextRequest) {
   }
 
   return NextResponse.next();
-}
-
-// トークンが期限切れかどうかを確認する関数
-function isTokenExpired(token: string): boolean {
-  console.log("isTokenExpired called");
-  try {
-    const payload = JSON.parse(atob(token.split(".")[1]));
-    const exp = payload.exp * 1000; // JWTの有効期限はUNIXタイムスタンプ（秒）なので、ミリ秒に変換
-    console.log("exp:", exp);
-    return Date.now() >= exp;
-  } catch (error) {
-    console.error("Token validation error:", error);
-    return true; // エラーが発生した場合は、安全のためトークンを期限切れとみなす
-  }
 }
 
 // ミドルウェアを適用するルートを指定
